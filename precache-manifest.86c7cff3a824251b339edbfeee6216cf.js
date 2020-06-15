ab35f5f7de42a8a7c71c899879dc6fecf39bb411
Pritam Ghosh <meghoshpritam@gmail.com>
2020-06-16 04:41:08 +0530

    Updates

---
+++ precache-manifest.86c7cff3a824251b339edbfeee6216cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d44a3572284719d443027d875bd9871",
+    "url": "/courses-client/index.html"
+  },
+  {
+    "revision": "91a3f4b63a6d715cec0c",
+    "url": "/courses-client/static/css/main.da6663f7.chunk.css"
+  },
+  {
+    "revision": "05fbd8b11def84d4565b",
+    "url": "/courses-client/static/js/2.a34650b7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/courses-client/static/js/2.a34650b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91a3f4b63a6d715cec0c",
+    "url": "/courses-client/static/js/main.7c407ff1.chunk.js"
+  },
+  {
+    "revision": "2652e92e1f9b144f25b1",
+    "url": "/courses-client/static/js/runtime-main.5e1e08b0.js"
+  }
+]);
