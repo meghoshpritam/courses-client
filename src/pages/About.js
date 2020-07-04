73c49fc8b401e3fa4701bc91adbc557b600d7324
Pritam Ghosh <meghoshpritam@gmail.com>
2020-07-05 01:34:36 +0530

    :construction: about page update

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,28 +1,53 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
+import { Link } from 'react-router-dom';
+import { makeStyles, emphasize } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  link: {
+    color: theme.palette.primary.dark,
+    textDecoration: 'none',
+    '&:hover': {
+      textDecoration: 'underline',
+    },
+  },
+  mainAbout: {
+    margin: theme.spacing(3, 0),
+  },
+}));
 
 export default () => {
+  const classes = useStyles();
+
   return (
-    <Container maxWidth="md">
+    <Container maxWidth="sm">
       <Container maxWidth="sm" style={{ textAlign: 'center', marginTop: 80, marginBottom: 15 }}>
         <Typography component="h1" variant="h2">
           About Us
         </Typography>
-        <Typography component="h2" variant="h6">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis quibusdam saepe omnis non
-          dolor consequatur, veritatis nostrum aliquam voluptas veniam, reprehenderit nihil!
-          Distinctio corporis dolore praesentium incidunt, minima cum voluptatem.
+        <Typography paragraph variant="body1" className={classes.mainAbout}>
+          <strong>L</strong>earn more to do more, with the best resources we teach you from start to
+          finish for in-depth knowledge about a subject. No more doubt, we provide live teacher
+          support for clear your doubts. Quick ways, tips and tricks are the part of out teaching.
         </Typography>
       </Container>
-      <Typography component="h2" variant="body1">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga, necessitatibus architecto
-        praesentium veniam omnis vitae aperiam, fugit illo ad libero ea eaque facere ducimus
-        quisquam veritatis corrupti dolores. Blanditiis necessitatibus voluptatibus culpa mollitia
-        sed amet inventore porro veniam repudiandae fuga voluptatem suscipit, repellendus totam
-        voluptas. Accusantium modi maxime esse quo itaque laboriosam repellendus laborum voluptas
-        iste! Vero velit facilis corrupti ratione voluptatum soluta, quia nam enim quidem quam iste
-        cum!
+      <Typography paragraph variant="body2">
+        <strong>A</strong>ll the people of our community including teacher, student, guides,
+        technical support teams are the part of our journey, We going towards a brighter future for
+        everyone. The world is continuously changing and update our courses accordingly, We teach
+        you the on-demand courses to achieve your goals. You will be an expert from the beginner.
+        Not too many courses and teachers to confuse you, we provide the best to you and save lots
+        of times by not to searching for a full and understandable course. The one platform for all
+        your education need. To start learning to{' '}
+        <Link to="/#explore" className={classes.link}>
+          explore our courses
+        </Link>{' '}
+        or join us{' '}
+        <Link to="/sign-up" className={classes.link}>
+          join us
+        </Link>{' '}
+        now to be a part of our community.
       </Typography>
     </Container>
   );
